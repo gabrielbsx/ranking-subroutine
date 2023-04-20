90b7c7ca7f32726a44b90cc31f1e44d54176e386
Gabriel Barbosa <gabrielturing@gmail.com>
2023-04-20 07:36:49 -0300

    feat: implements populate algorithm

--- src/core/algorithm/populate.ts
+++ src/core/algorithm/populate.ts
@@ -1,4 +1,7 @@
+import { join } from "ramda"
 import { Populate } from "./abstract-populate"
+import { readFileSync, readdirSync } from "node:fs"
+import console from "node:console"
 
 export class PopulateFileSystem extends Populate {
   private readonly _alphabeticFolderNames: string[] = new Array(26).fill(0).map((_, i) => String.fromCharCode(65 + i)).concat('etc')
@@ -6,7 +9,21 @@ export class PopulateFileSystem extends Populate {
 
   public populate(): void {
     this._alphabeticFolderNames.forEach((folderName) => {
-      console.log(folderName)
+      const absoluteFolder = join('/', [this.gamePath, folderName])
+      const accountFileNames = this.getListOfFilesOnPath(absoluteFolder)
+      accountFileNames.forEach((accountFileName) => {
+        this.readAccount(absoluteFolder, accountFileName)
+      })
     })
+  }
+
+  public async readAccount(folder: string, fileName: string): Promise<void> {
+    const absoluteAccountPath = join('/', [folder, fileName])
+    const accountBuffer = readFileSync(absoluteAccountPath)
+    console.log(accountBuffer.toString())
+  }
+
+  public getListOfFilesOnPath(folder: string): string[] {
+    return readdirSync(folder)
   }
 }
