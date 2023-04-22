b17a6e817c54dbb8dff8f6d4f90d0753a897d6a4
Gabriel Barbosa <gabrielturing@gmail.com>
2023-04-22 03:56:23 -0300

    test: ensure that load accounts chace validation is ok

--- src/tests/app/load-accounts.test.ts
+++ src/tests/app/load-accounts.test.ts
@@ -1,9 +1,9 @@
 import { beforeEach, describe, expect, test, vi } from 'vitest'
 import { cacheInMemory } from '../../core/algorithm/cache'
-import { accountStatsCacheAllocation } from '../../core/app/load-accounts'
-import { type Stats } from 'node:fs'
+import { accountStatsCacheAllocation, isAccountCached, isAccountChanged, isValidAccount } from '../../core/app/load-accounts'
+import { type StatsCache } from '../../core/contracts/account-stats-cache'
 
-const fileMock = {
+const fileMock: StatsCache = {
   size: 1,
   birthtime: new Date(),
   mtime: new Date(),
@@ -16,13 +16,37 @@ describe('load accoutns', () => {
   })
   test('accounts should be cached stats file if this data is not', () => {
     expect(cacheInMemory.data).toHaveLength(0)
-    accountStatsCacheAllocation(fileMock as Stats, 'any-account')
+    accountStatsCacheAllocation(fileMock, 'any-account')
     expect(cacheInMemory.data).toHaveLength(1)
   })
   test('accounts should be not cached stats file if file is directory', () => {
     expect(cacheInMemory.data).toHaveLength(0)
-    vi.spyOn(fileMock, 'isDirectory').mockReturnValue(true)
-    accountStatsCacheAllocation(fileMock as Stats, 'any-account')
+    const isDirectoryMocked = vi.spyOn(fileMock, 'isDirectory').mockReturnValue(true)
+    accountStatsCacheAllocation(fileMock, 'any-account')
     expect(cacheInMemory.data).toHaveLength(0)
+    expect(isValidAccount(fileMock)).toBe(false)
+    isDirectoryMocked.mockRestore()
+  })
+  test('accounts should be cached if file is changed or not', () => {
+    expect(cacheInMemory.data).toHaveLength(0)
+    const originalFileMock = fileMock
+    const modifiedFileMock = {
+      ...fileMock,
+      birthtime: new Date(),
+      mtime: new Date()
+    }
+    accountStatsCacheAllocation(originalFileMock, 'any-account')
+    expect(cacheInMemory.data).toHaveLength(1)
+    expect(isAccountChanged('any-account', modifiedFileMock)).toBe(true)
+    accountStatsCacheAllocation(modifiedFileMock, 'any-account')
+    expect(cacheInMemory.data).toHaveLength(1)
+    expect(isAccountChanged('any-account', modifiedFileMock)).toBe(false)
+  })
+  test('accounts should be validate if account stats is cached or not', () => {
+    expect(cacheInMemory.data).toHaveLength(0)
+    accountStatsCacheAllocation(fileMock, 'any-account')
+    expect(cacheInMemory.data).toHaveLength(1)
+    expect(isAccountCached('any-account')).toBe(true)
+    expect(isAccountCached('any-account-not-cached')).toBe(false)
   })
 })
