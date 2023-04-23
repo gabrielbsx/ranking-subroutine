1c36205523b2334a26e3ae65db789ff8b1bb8515
Gabriel Barbosa <gabrielturing@gmail.com>
2023-04-22 23:13:33 -0300

    chore: add pg client

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,5 +1,10 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     ACCOUNT_PATH: string
+    DB_HOST: string
+    DB_NAME: string
+    DB_USER: string
+    DB_PASS: string
+    DB_PORT: string
   }
 }
