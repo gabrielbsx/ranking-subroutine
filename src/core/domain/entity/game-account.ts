4a01ac21d6772adf575e39c3759c226088093b16
Gabriel Barbosa <gabrielturing@gmail.com>
2023-04-22 23:41:20 -0300

    feat: get account by username and cast _id to id in GameAccount return

---
+++ src/core/domain/entity/game-account.ts
@@ -0,0 +1,9 @@
+export interface GameAccount {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  userId: string
+  username: string
+  password: string
+  isMainAccount: boolean
+}
--- src/core/repository/account-repository.ts
+++ src/core/repository/account-repository.ts
@@ -1,15 +1,21 @@
 import { type Database } from '../algorithm/database-client'
+import { type GameAccount } from '../domain/entity/game-account'
 
 export interface AccountRepository {
-  getAccountByUsername: (username: string) => Promise<void>
+  getAccountByUsername: (username: string) => Promise<GameAccount>
 }
 
-export const accountRepository = (database: Database): AccountRepository => {
-  const databaseClient = database.connect()
+export const accountRepository = async (database: Database): Promise<AccountRepository> => {
+  const databaseClient = await database.connect()
 
-  const getAccountByUsername = async (username: string): Promise<void> => {
-    const account = await databaseClient.query('SELECT * FROM account WHERE username = $1', [username])
-    console.log(account)
+  const getAccountByUsername = async (username: string): Promise<GameAccount> => {
+    const databaseFetched = await databaseClient.query<GameAccount & { _id: string }>('SELECT * FROM "GameAccount" WHERE username ilike $1', [username])
+    const accountRow = databaseFetched.rows[0]
+    const account: GameAccount = {
+      ...accountRow,
+      id: accountRow._id
+    }
+    return account
   }
 
   return {
